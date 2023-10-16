e8f23389b3aa90af477748590b17ed3107ec49aa
Suraj Manik Lokhande <145440191+Surajlokhande0@users.noreply.github.com>
2023-10-16 19:23:32 +0530

    Add files via upload

---
+++ Python_coding/day 9.py
@@ -0,0 +1,18 @@
+def sortColors(nums):
+    low, mid, high = 0, 0, len(nums) - 1
+    
+    while mid <= high:
+        if nums[mid] == 0:
+            nums[low], nums[mid] = nums[mid], nums[low]
+            low += 1
+            mid += 1
+        elif nums[mid] == 1:
+            mid += 1
+        else:
+            nums[mid], nums[high] = nums[high], nums[mid]
+            high -= 1
+
+# Example usage:
+array = [2, 0, 1, 1, 0, 2]
+sortColors(array)
+print(array)
